1db2c9bfc894cbe4f19830804c9ce8bf8a7df681
gaspno <zatchgabriel@gmail.com>
2021-09-08 13:35:53 -0300

    Layers

---
+++ backend/dsvendas/src/main/java/com/devsuperior/dsvendas/dto/SellerDto.java
@@ -0,0 +1,51 @@
+package com.devsuperior.dsvendas.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dsvendas.entities.Seller;
+
+public class SellerDto implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long Id;
+	private String name;
+	
+	public SellerDto()
+	{
+		
+	}
+
+	public SellerDto(Long id, String name) {
+		super();
+		Id = id;
+		this.name = name;
+	}
+	
+	public SellerDto(Seller entity) {
+		super();
+		Id = entity.getId();
+		this.name = entity.getName();
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
+
